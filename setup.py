6e0a48930fc458a710ac65e4b1a41b9dc3815df7
Christoph Schueler <cpu12.gems@googlemail.com>
2018-03-04 14:33:15 +0100

    Add dependencies to pyserial.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author_email = 'cpu12.gems@googlemail.com',
     url = 'http://github.com/pySART/pyxcp',
     packages = packagez('pyxcp'),
-    install_requires = ['enum34', 'construct >= 2.8', 'mako'],
+    install_requires = ['enum34', 'construct >= 2.8', 'mako', 'pyserial'],
     package_dir = {'tests': 'pyxcp/tests'},
     test_suite = "pyxcp.tests"
 )
