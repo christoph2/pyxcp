ed90ce86c3851971418462d9e758d733b04b910b
Christoph Schueler <cpu12.gems@googlemail.com>
2018-03-22 08:33:04 +0100

    Add doc strings.

--- pyxcp/asam/types.py
+++ pyxcp/asam/types.py
@@ -1,3 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__copyright__="""
+    pySART - Simplified AUTOSAR-Toolkit for Python.
+
+   (C) 2009-2018 by Christoph Schueler <cpu12.gems@googlemail.com>
+
+   All Rights Reserved
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
 
 import struct
 
@@ -7,17 +31,6 @@ MOTOROLA = ">"
 """
     A_VOID: pseudo type for non-existing elements
     A_BIT: one bit
-
-    A_UNIT8: unsigned integer 8-bit
-    A_UINT16: unsigned integer 16-bit
-    A_UINT32: unsigned integer 32-bit
-    A_INT8: signed integer 8-bit, two's complement
-    A_INT16: signed integer 16-bit, two's complement
-    A_INT32: signed integer 32-bit, two's complement
-    A_INT64: signed integer 64-bit, two's complement
-    A_FLOAT32: IEEE 754 single precision
-    A_FLOAT64: IEEE 754 double precision
-
     A_ASCIISTRING: string, ISO-8859-1 encoded
     A_UTF8STRING: string, UTF-8 encoded
     A_UNICODE2STRING: string, UCS-2 encoded
@@ -25,55 +38,116 @@ MOTOROLA = ">"
 """
 
 class AsamBaseType(object):
+    """Base class for ASAM codecs.
 
-  def __init__(self, byteorder):
-    if not  byteorder in ("<", ">"):
-        raise TypeError("Invalid byteorder.")
-    self.byteorder = byteorder
+    Note
+    ----
+    Always use derived classes.
+    """
 
-  def encode(self, value):
-    return struct.pack("{}{}".format(self.byteorder, self.FMT), value)
+    def __init__(self, byteorder):
+        """
 
-  def decode(self, value):
-    return struct.unpack("{}{}".format(self.byteorder, self.FMT), bytes(value))[0]
+        Parameters
+        ----------
+        byteorder: char {'<', '>'}
+          - '<' Little-endian
+          - '>' Big-endian
+        """
+        if not  byteorder in ("<", ">"):
+            raise TypeError("Invalid byteorder.")
+        self.byteorder = byteorder
+
+    def encode(self, value):
+        """Encode a value.
+
+        Encode means convert a value, eg. an integer, to a byte-string.
+
+        Parameters
+        ----------
+        value: data-type
+          data-type is determined by derived class.
+
+        Returns
+        -------
+        bytes
+          Encoded value.
+        """
+        return struct.pack("{}{}".format(self.byteorder, self.FMT), value)
+
+    def decode(self, value):
+        """Decode a value.
+
+        Decode means convert a byte-string to a meaningful data-type, eg. an integer.
+
+        Parameters
+        ----------
+        value:  bytes
+
+        Returns
+        -------
+        data-type
+          data-type is determined by derived class.
+        """
+        return struct.unpack("{}{}".format(self.byteorder, self.FMT), bytes(value))[0]
 
 
 class A_Uint8(AsamBaseType):
-  FMT = "B"
+    """ASAM A_UINT8 codec.
+    """
+    FMT = "B"
 
 
 class A_Uint16(AsamBaseType):
-  FMT = "H"
+    """ASAM A_UINT16 codec.
+    """
+    FMT = "H"
 
 
 class A_Uint32(AsamBaseType):
-  FMT = "I"
+    """ASAM A_UINT32 codec.
+    """
+    FMT = "I"
 
 
 class A_Uint64(AsamBaseType):
-  FMT = "Q"
+    """ASAM A_UINT64 codec.
+    """
+    FMT = "Q"
 
 
 class A_Int8(AsamBaseType):
-  FMT = "b"
+    """ASAM A_INT8 codec.
+    """
+    FMT = "b"
 
 
 class A_Int16(AsamBaseType):
-  FMT = "h"
+    """ASAM A_INT16 codec.
+    """
+    FMT = "h"
 
 
 class A_Int32(AsamBaseType):
-  FMT = "i"
+    """ASAM A_INT32 codec.
+    """
+    FMT = "i"
 
 
 class A_Int64(AsamBaseType):
-  FMT = "q"
+    """ASAM A_INT64 codec.
+    """
+    FMT = "q"
 
 
 class A_Float32(AsamBaseType):
-  FMT = "f"
+    """ASAM A_FLOAT32 codec.
+    """
+    FMT = "f"
 
 
 class A_Float64(AsamBaseType):
-  FMT = "d"
+    """ASAM A_FLOAT64 codec.
+    """
+    FMT = "d"
 
