5e2d98146963c2bcbea4bf7d661271a4f86289ad
Christoph Schueler <cpu12.gems@googlemail.com>
2018-03-22 08:32:37 +0100

    Add byte-order tests.

--- pyxcp/tests/asamtypes.py
+++ pyxcp/tests/asamtypes.py
@@ -3,17 +3,28 @@ import unittest
 
 from pyxcp.asam import types
 
-class Encode(unittest.TestCase):
+class TestEncode(unittest.TestCase):
 
-  def testEncodeUint32_0(self):
-    self.assertEqual(types.A_Uint32("<").encode(3415750566), b'\xa67\x98\xcb')
+    def testEncodeUint32_0(self):
+        self.assertEqual(types.A_Uint32("<").encode(3415750566), b'\xa67\x98\xcb')
 
 
-class Decode(unittest.TestCase):
+class TestDecode(unittest.TestCase):
 
-  def testDecodeUint32_0(self):
-    self.assertEqual(types.A_Uint32("<").decode((0xa6, 0x37, 0x98, 0xcb)), 3415750566)
-    
+    def testDecodeUint32_0(self):
+        self.assertEqual(types.A_Uint32("<").decode((0xa6, 0x37, 0x98, 0xcb)), 3415750566)
+
+
+class TestAsamBaseType(unittest.TestCase):
+
+    def testLittleEndian(self):
+        self.assertTrue(isinstance(types.AsamBaseType(types.INTEL), types.AsamBaseType))
+
+    def testBigEndian(self):
+        self.assertTrue(isinstance(types.AsamBaseType(types.MOTOROLA), types.AsamBaseType))
+
+    def testInvalidByteOrderRaisesTypeError(self):
+        self.assertRaises(TypeError, types.AsamBaseType, '#')
 
 if __name__ == '__main__':
   unittest.main()
