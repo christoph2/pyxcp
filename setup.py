adca50cb7c1e0f8a73090ee6352fb76c64a6fe51
Christoph Schueler <cpu12.gems@googlemail.com>
2019-01-08 21:11:57 +0100

    No Python 3.7 for now

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ classifiers=[
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+#        'Programming Language :: Python :: 3.7',
     ],
 )
 
