2969261e2109d12e39673e61ba1bf4859130d0c6
Felix Nieuwenhuizen <felix@tdlrali.com>
2019-02-09 13:50:47 +0100

    switch test_master.py to pytest

--- pyxcp/tests/test_master.py
+++ pyxcp/tests/test_master.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import unittest
 from unittest import mock
 
 from pyxcp.master import Master
 from pyxcp import transport
 
 
-class TestMaster(unittest.TestCase):
+class TestMaster:
 
     @mock.patch("pyxcp.transport.Eth")
     def testConnect(self, Eth):
@@ -17,20 +16,20 @@ class TestMaster(unittest.TestCase):
             [0x1d, 0xc0, 0xff, 0xdc, 0x05, 0x01, 0x01])
         with Master(tr) as xm:
             res = xm.connect()
-        self.assertEqual(res.maxCto, 255)
-        self.assertEqual(res.maxDto, 1500)
-        self.assertEqual(res.protocolLayerVersion, 1)
-        self.assertEqual(res.transportLayerVersion, 1)
-        self.assertEqual(res.resource.pgm, True)
-        self.assertEqual(res.resource.stim, True)
-        self.assertEqual(res.resource.daq, True)
-        self.assertEqual(res.resource.calpag, True)
-        self.assertEqual(res.commModeBasic.optional, True)
-        self.assertEqual(res.commModeBasic.slaveBlockMode, True)
-        self.assertEqual(res.commModeBasic.addressGranularity, 'BYTE')
-        self.assertEqual(res.commModeBasic.byteOrder, 'INTEL')
-        self.assertEqual(xm.maxCto, res.maxCto)
-        self.assertEqual(xm.maxDto, res.maxDto)
+        assert res.maxCto == 255
+        assert res.maxDto == 1500
+        assert res.protocolLayerVersion == 1
+        assert res.transportLayerVersion == 1
+        assert res.resource.pgm is True
+        assert res.resource.stim is True
+        assert res.resource.daq is True
+        assert res.resource.calpag is True
+        assert res.commModeBasic.optional is True
+        assert res.commModeBasic.slaveBlockMode is True
+        assert res.commModeBasic.addressGranularity == 'BYTE'
+        assert res.commModeBasic.byteOrder == 'INTEL'
+        assert xm.maxCto == res.maxCto
+        assert xm.maxDto == res.maxDto
 
     @mock.patch("pyxcp.transport.Eth")
     def testDisconnect(self, Eth):
@@ -38,7 +37,7 @@ class TestMaster(unittest.TestCase):
         tr.request.return_value = bytes([])
         with Master(tr) as xm:
             res = xm.disconnect()
-        self.assertEqual(res, b'')
+        assert res == b''
 
     @mock.patch("pyxcp.transport.Eth")
     def testGetStatus(self, Eth):
@@ -46,16 +45,16 @@ class TestMaster(unittest.TestCase):
         tr.request.return_value = bytes([0x00, 0x1d, 0xff, 0x00, 0x00])
         with Master(tr) as xm:
             res = xm.getStatus()
-        self.assertEqual(res.sessionConfiguration, 0)
-        self.assertEqual(res.sessionStatus.resume, False)
-        self.assertEqual(res.sessionStatus.daqRunning, False)
-        self.assertEqual(res.sessionStatus.clearDaqRequest, False)
-        self.assertEqual(res.sessionStatus.storeDaqRequest, False)
-        self.assertEqual(res.sessionStatus.storeCalRequest, False)
-        self.assertEqual(res.resourceProtectionStatus.pgm, True)
-        self.assertEqual(res.resourceProtectionStatus.stim, True)
-        self.assertEqual(res.resourceProtectionStatus.daq, True)
-        self.assertEqual(res.resourceProtectionStatus.calpag, True)
+        assert res.sessionConfiguration == 0
+        assert res.sessionStatus.resume is False
+        assert res.sessionStatus.daqRunning is False
+        assert res.sessionStatus.clearDaqRequest is False
+        assert res.sessionStatus.storeDaqRequest is False
+        assert res.sessionStatus.storeCalRequest is False
+        assert res.resourceProtectionStatus.pgm is True
+        assert res.resourceProtectionStatus.stim is True
+        assert res.resourceProtectionStatus.daq is True
+        assert res.resourceProtectionStatus.calpag is True
 
     @mock.patch("pyxcp.transport.Eth")
     def testSync(self, Eth):
@@ -63,7 +62,7 @@ class TestMaster(unittest.TestCase):
         tr.request.return_value = bytes([0x00])
         with Master(tr) as xm:
             res = xm.synch()
-        self.assertEqual(len(res), 1)
+        assert len(res) == 1
 
     @mock.patch("pyxcp.transport.Eth")
     def testGetCommModeInfo(self, Eth):
@@ -72,12 +71,12 @@ class TestMaster(unittest.TestCase):
             [0x00, 0x01, 0xff, 0x02, 0x00, 0x00, 0x19])
         with Master(tr) as xm:
             res = xm.getCommModeInfo()
-        self.assertEqual(res.commModeOptional.interleavedMode, False)
-        self.assertEqual(res.commModeOptional.masterBlockMode, True)
-        self.assertEqual(res.maxbs, 2)
-        self.assertEqual(res.minSt, 0)
-        self.assertEqual(res.queueSize, 0)
-        self.assertEqual(res.xcpDriverVersionNumber, 25)
+        assert res.commModeOptional.interleavedMode is False
+        assert res.commModeOptional.masterBlockMode is True
+        assert res.maxbs == 2
+        assert res.minSt == 0
+        assert res.queueSize == 0
+        assert res.xcpDriverVersionNumber == 25
 
     @mock.patch("pyxcp.transport.Eth")
     def testGetID(self, Eth):
@@ -89,13 +88,13 @@ class TestMaster(unittest.TestCase):
             tr.request.return_value = bytes(
                 [0x58, 0x43, 0x50, 0x73, 0x69, 0x6d])
             res = xm.upload(gid.length)
-        self.assertEqual(gid.mode, 0)
-        self.assertEqual(gid.reserved, 65281)
-        self.assertEqual(gid.length, 6)
-        self.assertEqual(res, b'XCPsim')
+        assert gid.mode == 0
+        assert gid.reserved == 65281
+        assert gid.length == 6
+        assert res == b'XCPsim'
 
-    @mock.patch('socket.socket')
-    @mock.patch('selectors.DefaultSelector')
+    @mock.patch('pyxcp.transport.eth.socket.socket')
+    @mock.patch('pyxcp.transport.eth.selectors.DefaultSelector')
     def testConnect2(self, mock_selector, mock_socket):
         mock_socket.return_value.recv.side_effect = [
             [0x08, 0x00, 0x00, 0x00],
@@ -105,26 +104,26 @@ class TestMaster(unittest.TestCase):
         with Master(transport.Eth('localhost', loglevel="DEBUG")) as xm:
             res = xm.connect()
 
-        self.assertEqual(res.maxCto, 255)
-        self.assertEqual(res.maxDto, 1500)
-        self.assertEqual(res.protocolLayerVersion, 1)
-        self.assertEqual(res.transportLayerVersion, 1)
-        self.assertEqual(res.resource.pgm, True)
-        self.assertEqual(res.resource.stim, True)
-        self.assertEqual(res.resource.daq, True)
-        self.assertEqual(res.resource.calpag, True)
-        self.assertEqual(res.commModeBasic.optional, True)
-        self.assertEqual(res.commModeBasic.slaveBlockMode, True)
-        self.assertEqual(res.commModeBasic.addressGranularity, 'BYTE')
-        self.assertEqual(res.commModeBasic.byteOrder, 'INTEL')
-        self.assertEqual(xm.maxCto, res.maxCto)
-        self.assertEqual(xm.maxDto, res.maxDto)
+        assert res.maxCto == 255
+        assert res.maxDto == 1500
+        assert res.protocolLayerVersion == 1
+        assert res.transportLayerVersion == 1
+        assert res.resource.pgm is True
+        assert res.resource.stim is True
+        assert res.resource.daq is True
+        assert res.resource.calpag is True
+        assert res.commModeBasic.optional is True
+        assert res.commModeBasic.slaveBlockMode is True
+        assert res.commModeBasic.addressGranularity == 'BYTE'
+        assert res.commModeBasic.byteOrder == 'INTEL'
+        assert xm.maxCto == res.maxCto
+        assert xm.maxDto == res.maxDto
 
         mock_socket.return_value.send.assert_called_with(bytes(
             [0x02, 0x00, 0x00, 0x00, 0xff, 0x00]))
 
-    @mock.patch('socket.socket')
-    @mock.patch('selectors.DefaultSelector')
+    @mock.patch('pyxcp.transport.eth.socket.socket')
+    @mock.patch('pyxcp.transport.eth.selectors.DefaultSelector')
     def testDisconnect2(self, mock_selector, mock_socket):
         mock_socket.return_value.recv.side_effect = [
             [0x01, 0x00, 0x00, 0x00], [0xff]]
@@ -133,13 +132,13 @@ class TestMaster(unittest.TestCase):
         with Master(transport.Eth('localhost', loglevel="DEBUG")) as xm:
             res = xm.disconnect()
 
-        self.assertEqual(res, b'')
+        assert res == b''
 
         mock_socket.return_value.send.assert_called_with(bytes(
             [0x01, 0x00, 0x00, 0x00, 0xfe]))
 
-    @mock.patch('socket.socket')
-    @mock.patch('selectors.DefaultSelector')
+    @mock.patch('pyxcp.transport.eth.socket.socket')
+    @mock.patch('pyxcp.transport.eth.selectors.DefaultSelector')
     def testGetStatus2(self, mock_selector, mock_socket):
         mock_socket.return_value.recv.side_effect = [
             [0x06, 0x00, 0x00, 0x00],
@@ -149,22 +148,22 @@ class TestMaster(unittest.TestCase):
         with Master(transport.Eth('localhost', loglevel="DEBUG")) as xm:
             res = xm.getStatus()
 
-        self.assertEqual(res.sessionStatus.storeCalRequest, True)
-        self.assertEqual(res.sessionStatus.storeDaqRequest, False)
-        self.assertEqual(res.sessionStatus.clearDaqRequest, True)
-        self.assertEqual(res.sessionStatus.daqRunning, False)
-        self.assertEqual(res.sessionStatus.resume, False)
-        self.assertEqual(res.resourceProtectionStatus.pgm, True)
-        self.assertEqual(res.resourceProtectionStatus.stim, True)
-        self.assertEqual(res.resourceProtectionStatus.daq, True)
-        self.assertEqual(res.resourceProtectionStatus.calpag, True)
-        self.assertEqual(res.sessionConfiguration, 0x1234)
+        assert res.sessionStatus.storeCalRequest is True
+        assert res.sessionStatus.storeDaqRequest is False
+        assert res.sessionStatus.clearDaqRequest is True
+        assert res.sessionStatus.daqRunning is False
+        assert res.sessionStatus.resume is False
+        assert res.resourceProtectionStatus.pgm is True
+        assert res.resourceProtectionStatus.stim is True
+        assert res.resourceProtectionStatus.daq is True
+        assert res.resourceProtectionStatus.calpag is True
+        assert res.sessionConfiguration == 0x1234
 
         mock_socket.return_value.send.assert_called_with(bytes(
             [0x01, 0x00, 0x00, 0x00, 0xfd]))
 
-    @mock.patch('socket.socket')
-    @mock.patch('selectors.DefaultSelector')
+    @mock.patch('pyxcp.transport.eth.socket.socket')
+    @mock.patch('pyxcp.transport.eth.selectors.DefaultSelector')
     def testSynch(self, mock_selector, mock_socket):
         mock_socket.return_value.recv.side_effect = [
             [0x02, 0x00, 0x00, 0x00],
@@ -174,13 +173,13 @@ class TestMaster(unittest.TestCase):
         with Master(transport.Eth('localhost', loglevel="DEBUG")) as xm:
             res = xm.synch()
 
-        self.assertEqual(res, b'\x00')
+        assert res == b'\x00'
 
         mock_socket.return_value.send.assert_called_with(bytes(
             [0x01, 0x00, 0x00, 0x00, 0xfc]))
 
-    @mock.patch('socket.socket')
-    @mock.patch('selectors.DefaultSelector')
+    @mock.patch('pyxcp.transport.eth.socket.socket')
+    @mock.patch('pyxcp.transport.eth.selectors.DefaultSelector')
     def testGetCommModeInfo2(self, mock_selector, mock_socket):
         mock_socket.return_value.recv.side_effect = [
             [0x08, 0x00, 0x00, 0x00],
@@ -190,18 +189,18 @@ class TestMaster(unittest.TestCase):
         with Master(transport.Eth('localhost', loglevel="DEBUG")) as xm:
             res = xm.getCommModeInfo()
 
-        self.assertEqual(res.commModeOptional.interleavedMode, False)
-        self.assertEqual(res.commModeOptional.masterBlockMode, True)
-        self.assertEqual(res.maxbs, 2)
-        self.assertEqual(res.minSt, 0)
-        self.assertEqual(res.queueSize, 0)
-        self.assertEqual(res.xcpDriverVersionNumber, 25)
+        assert res.commModeOptional.interleavedMode is False
+        assert res.commModeOptional.masterBlockMode is True
+        assert res.maxbs == 2
+        assert res.minSt == 0
+        assert res.queueSize == 0
+        assert res.xcpDriverVersionNumber == 25
 
         mock_socket.return_value.send.assert_called_with(bytes(
             [0x01, 0x00, 0x00, 0x00, 0xfb]))
 
-    @mock.patch('socket.socket')
-    @mock.patch('selectors.DefaultSelector')
+    @mock.patch('pyxcp.transport.eth.socket.socket')
+    @mock.patch('pyxcp.transport.eth.selectors.DefaultSelector')
     def testGetID2(self, mock_selector, mock_socket):
         mock_socket.return_value.recv.side_effect = [
             [0x08, 0x00, 0x00, 0x00],
@@ -222,13 +221,13 @@ class TestMaster(unittest.TestCase):
             mock_socket.return_value.send.assert_called_with(bytes(
                 [0x02, 0x00, 0x01, 0x00, 0xf5, 0x06]))
 
-        self.assertEqual(gid.mode, 0)
-        self.assertEqual(gid.reserved, 65281)
-        self.assertEqual(gid.length, 6)
-        self.assertEqual(res, b'XCPsim')
+        assert gid.mode == 0
+        assert gid.reserved == 65281
+        assert gid.length == 6
+        assert res == b'XCPsim'
 
-    @mock.patch('socket.socket')
-    @mock.patch('selectors.DefaultSelector')
+    @mock.patch('pyxcp.transport.eth.socket.socket')
+    @mock.patch('pyxcp.transport.eth.selectors.DefaultSelector')
     def testDownloadMax(self, mock_selector, mock_socket):
         mock_socket.return_value.recv.side_effect = [
             [0x01, 0x00, 0x00, 0x00], [0xff]]
@@ -247,11 +246,3 @@ E   Actual call:    send(b'\x02\x00\x00\x00\xee\xca\xfe\xba\xbe')
 
         mock_socket.return_value.send.assert_called_with(bytes(
             [0x05, 0x00, 0x00, 0x00, 0xee, 0xca, 0xfe, 0xba, 0xbe]))
-
-
-def main():
-    unittest.main()
-
-
-if __name__ == '__main__':
-    main()
