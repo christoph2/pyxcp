31bdc8a632b2d1814d6447378aad74abd85a5e31
Christoph Schueler <cpu12.gems@googlemail.com>
2019-02-10 13:17:11 +0100

    Add napoleon settings.

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ release = '0.9'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
-#    'sphinx.ext.todo'    
+#    'sphinx.ext.todo'
     'sphinxcontrib.napoleon',
 ]
 
@@ -165,3 +165,19 @@ texinfo_documents = [
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+
+# Napoleon settings
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = False
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_use_keyword = True
+napoleon_custom_sections = None
