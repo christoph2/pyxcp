aca585fe5682284b73e63a887965d785962f87cc
Christoph Schueler <cpu12.gems@googlemail.com>
2019-03-08 13:06:52 +0100

    Don't support Python < 3.4

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/bin/env python
 
 import os
-from setuptools import find_packages, setup
 import sys
+from setuptools import find_packages, setup
 
 with open(os.path.join('pyxcp', 'version.py'), 'r') as f:
     for line in f:
@@ -14,10 +14,6 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 install_reqs = ['construct >= 2.9.0', 'mako', 'pyserial', 'sphinxcontrib-napoleon']
-
-if sys.version_info.major == 2 or (
-        sys.version_info.major == 3 and sys.version_info.minor < 4):
-    install_reqs.extend(['enum34', 'mock'])
 
 setup(
     name='pyxcp',
@@ -56,6 +52,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
