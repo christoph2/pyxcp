a6c6c9decb0c944bcd3fe0dda500473f448fe8d0
Christoph Schueler <cpu12.gems@googlemail.com>
2019-12-05 00:32:13 +0100

    Fix typo.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 install_reqs = [
-    'construct >= 2.9.0', 'mako', 'pyserial', 'toml', 'pysub']
+    'construct >= 2.9.0', 'mako', 'pyserial', 'toml', 'pyusb']
 
 setup(
     name='pyxcp',
