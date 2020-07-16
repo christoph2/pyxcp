806ea8f170b586fc1808f08b6c227f16d1f954a0
Christoph Schueler <cpu12.gems@googlemail.com>
2020-07-16 15:31:53 +0200

    Add xcp_unlock.py

---
+++ pyxcp/examples/xcp_unlock.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Very basic hello-world example.
+"""
+
+__copyright__ = """
+    pySART - Simplified AUTOSAR-Toolkit for Python.
+
+   (C) 2020 by Christoph Schueler <cpu12.gems@googlemail.com>
+
+   All Rights Reserved
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+"""
+"""
+
+from pyxcp.cmdline import ArgumentParser
+
+
+ap = ArgumentParser(description = "XCP slave UNLOCK demonstration.")
+
+
+SEED_N_KEY_DLL = "SeedNKeyXcp.dll"    # Hardcoded for now.
+
+with ap.run() as x:
+    x.seedNKeyDLL = SEED_N_KEY_DLL
+    x.connect()
+
+    print("")
+    rps = x.getCurrentProtectionStatus()
+    print("Protection before unlocking:", rps, end = "\n\n")
+
+    x.cond_unlock()
+
+    rps = x.getCurrentProtectionStatus()
+    print("Protection after unlocking:",  rps)
+
+    x.disconnect()
+
