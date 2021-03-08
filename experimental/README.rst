ed7dcfa33a8060dc04c9849c49230ba9dd6e4148
Christoph Schueler <cpu12.gems@googlemail.com>
2021-03-08 16:39:09 +0100

    Update config.h and README.rst

--- experimental/README.rst
+++ experimental/README.rst
@@ -2,6 +2,6 @@
 Experimental Eth stuff
 ======================
 
-# ... benchmarks to highlight the performance differences between C and Python.
+- Codebase for the forthcoming Eth performance booster.
+- C and Python clients and servers to highlight the performance differences between C and Python (*benchmark/*).
 
-# code base for the forthcoming Eth performance booster.
--- experimental/eth/benchmark/config.h
+++ experimental/eth/benchmark/config.h
@@ -1,19 +1,25 @@
 
+#if !defined(__CONFIG_H)
+#define __CONFIG_H
+/*
+ * Basic Options.
+ */
 #define PORT "56789"
 #define HOST "localhost"
+#define MIN_MSG_LEN     (128)
+#define MAX_MSG_LEN     (16384)
+#define TOTAL_BYTES     (250 * 1024 * 1024)
 
+/*
+ * Performance Relevant Options.
+ */
 #define NO_DELAY        (1)
 #define SOCKET_SNDBUF   (4096 * 8)
 #define SOCKET_RCVBUF   (4096 * 4)
 #define RCV_RCVBUF      (4096 * 1)
 
-#define TOTAL_BYTES     (250 * 1024 * 1024)
-
-#define MIN_MSG_LEN     (128)
-#define MAX_MSG_LEN     (16384)
-
-#define MIN_FRAGMENTS   (4)
-#define MAX_FRAGMENTS   (64)
 
 #define SIZEOF_SHORT    (sizeof(unsigned short))
+
+#endif // __CONFIG_H
 
