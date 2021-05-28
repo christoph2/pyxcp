bdf617368e60800cf67f40a742e0eee2e2580561
Christoph Schueler <cpu12.gems@googlemail.com>
2021-05-28 15:58:59 +0200

    Use SO_REUSEADDR option.

--- pyxcp/transport/cxx_ext/blocking_client.cpp
+++ pyxcp/transport/cxx_ext/blocking_client.cpp
@@ -18,9 +18,12 @@ std::array<char, 15> hellomsg {"hello world!!!"};
 
 int main(void)
 {
-
+    int opt;
     CAddress address;
     auto sock = Socket {PF_INET, SOCK_STREAM, IPPROTO_TCP};
+
+    opt = 1;
+    sock.option(SO_REUSEADDR, SOL_SOCKET, &opt);
 
     //sock.getaddrinfo(PF_INET, SOCK_STREAM, IPPROTO_TCP, "localhost", 50007, address, 0);
     sock.getaddrinfo(PF_INET, SOCK_STREAM, IPPROTO_TCP, "192.168.168.100", 50007, address, 0);
