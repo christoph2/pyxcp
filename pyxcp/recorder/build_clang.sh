127e0408e62422f5ee90b137825fbc2722f612a1
Christoph Schueler <cpu12.gems@googlemail.com>
2022-08-12 11:14:09 +0200

    Update recorder build scripts

--- pyxcp/recorder/build_clang.sh
+++ pyxcp/recorder/build_clang.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-clang++ -std=c++17 -O3 -fvectorize -Rpass=loop-vectorize -ggdb -Wall -Wextra -Weffc++ -DLZ4_DEBUG=1 -DSTANDALONE_REKORDER=1 -I/usr/local/include/python3.7m -I/usr/local/include/python3.7m -I/usr/local/include/python3.7m -I/home/pi/.local/lib/python3.7/site-packages/pybind11/include lz4.cpp rekorder.cpp -o rekorder
+clang++ -std=c++20 -O3 -fvectorize -Rpass=loop-vectorize -ggdb -Wall -Wextra -Weffc++ -DLZ4_DEBUG=1 -DSTANDALONE_REKORDER=1 -I/usr/local/include/python3.7m -I/usr/local/include/python3.7m -I/usr/local/include/python3.7m -I/home/pi/.local/lib/python3.7/site-packages/pybind11/include lz4.cpp rekorder.cpp -o rekorder
