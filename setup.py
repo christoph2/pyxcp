f49475b7a2d6edfdf4dfa374f96c6e8058fe6c1e
Christoph Schueler <cpu12.gems@googlemail.com>
2022-09-11 14:22:46 +0200

    Update xcphello.py and also make it available as xcp_info script

--- setup.py
+++ setup.py
@@ -150,6 +150,7 @@ setuptools.setup(
             "pyxcp-probe-can-drivers = pyxcp.scripts.pyxcp_probe_can_drivers:main",
             "xcp-id-scanner = pyxcp.scripts.xcp_id_scanner:main",
             "xcp-fetch-a2l = pyxcp.scripts.xcp_fetch_a2l:main",
+            "xcp-info = pyxcp.scripts.xcp_info:main",
         ],
     },
 )
