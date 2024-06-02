63c39b9c420038c15b92970d5f3a8748d4bdc7d9
Christoph Schueler <cpu12.gems@googlemail.com>
2024-06-02 21:31:44 +0300

    today

--- pyxcp/transport/sxi.py
+++ pyxcp/transport/sxi.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 
+
 import struct
-from time import time
+import threading
+from array import array
+from collections import deque
+from time import sleep, time
 
 import serial
 
 import pyxcp.types as types
 from pyxcp.transport.base import BaseTransport
+
+
+RECV_SIZE = 16384
 
 
 class SxI(BaseTransport):
@@ -30,24 +37,30 @@ class SxI(BaseTransport):
         self.framing = self.config.framing
         self.esc_sync = self.config.esc_sync
         self.esc_esc = self.config.esc_esc
+        self.logger.debug(f"Trying to open serial comm_port {self.port_name}.")
+        try:
+            self.comm_port = serial.Serial(
+                port=self.port_name,
+                baudrate=self.baudrate,
+                bytesize=self.bytesize,
+                parity=self.parity,
+                stopbits=self.stopbits,
+                timeout=self.timeout,
+            )
+        except serial.SerialException as e:
+            self.logger.critical(f"{e}")
+            raise
+        self._packet_listener = threading.Thread(
+            target=self._packet_listen,
+            args=(),
+            kwargs={},
+        )
+        self._packets = deque()
 
     def __del__(self):
         self.closeConnection()
 
     def connect(self):
-        self.logger.debug(f"Trying to open serial comm_port {self.port_name}.")
-        try:
-            self.comm_port = serial.Serial()
-            self.comm_port.port = self.port_name
-            self.comm_port.baudrate = self.baudrate
-            self.comm_port.bytesize = self.bytesize
-            self.comm_port.parity = self.parity
-            self.comm_port.stopbits = self.stopbits
-            self.comm_port.timeout = self.timeout
-            self.comm_port.open()
-        except serial.SerialException as e:
-            self.logger.critical(f"{e}")
-            raise
         self.logger.info(f"Serial comm_port openend as {self.comm_port.portstr}@{self.baudrate} Bits/Sec.")
         self.startListener()
 
@@ -61,6 +74,50 @@ class SxI(BaseTransport):
 
     def flush(self):
         self.comm_port.flush()
+
+    def _packet_listen(self):
+        print("PACKET-LISTEN")
+        close_event_set = self.closeEvent.is_set
+
+        _packets = self._packets
+        read = self.comm_port.read
+
+        buffer = array("B", bytes(RECV_SIZE))
+        buffer_view = memoryview(buffer)
+
+        while True:
+            try:
+                print("Trying...")
+                if close_event_set():
+                    print("close_event_set()")
+                    return
+                print("Enter reader...")
+                recv_timestamp = time()
+                sleep(0.1)
+                ra = self.comm_port.read_all()
+                print("*** READ-ALL", ra)
+                read_count = read(buffer, 10)  # 100ms timeout
+                print("RC", read_count)
+                if read_count != RECV_SIZE:
+                    _packets.append((buffer_view[:read_count].tobytes(), recv_timestamp))
+                else:
+                    _packets.append((buffer.tobytes(), recv_timestamp))
+                    # except (USBError, USBTimeoutError):
+                    # print(format_exc())
+                    # sleep(SHORT_SLEEP)
+                    continue
+            except BaseException:  # noqa: B036
+                # Note: catch-all only permitted if the intention is re-raising.
+                self.status = 0  # disconnected
+                break
+
+    def startListener(self):
+        super().startListener()
+        print("*** START LISTENER ***")
+        if self._packet_listener.is_alive():
+            self._packet_listener.join()
+        self._packet_listener = threading.Thread(target=self._packet_listen)
+        self._packet_listener.start()
 
     def listen(self):
         while True:
@@ -77,7 +134,7 @@ class SxI(BaseTransport):
 
             if len(response) != length:
                 raise types.FrameSizeError("Size mismatch.")
-
+            print("\t***RESP", response)
             self.processResponse(response, length, counter, recv_timestamp)
 
     def send(self, frame):
