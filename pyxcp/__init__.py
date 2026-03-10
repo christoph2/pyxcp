887eb6985ea00aad0fccc106ab6c3a314e24a3ae
Christoph Schueler <cpu12.gems@googlemail.com>
2026-03-10 18:48:26 +0200

    bump version 0.28.1 -> 0.29.0

--- pyxcp/__init__.py
+++ pyxcp/__init__.py
@@ -19,7 +19,7 @@ tb_install(show_locals=True, max_frames=3)  # Install custom exception handler.
 
 # if you update this manually, do not forget to update
 # pyproject.toml.
-__version__ = "0.28.1"
+__version__ = "0.29.0"
 
 
 # Deprecation warning for TOML config
